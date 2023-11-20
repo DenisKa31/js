4ca8c8bf763c08b3abc4fc259ebe73840a80ca0b
Denis <146764711+DenisKa31@users.noreply.github.com>
2023-11-21 00:16:13 +0200

    Add files via upload

---
+++ exercise1.js
@@ -0,0 +1,54 @@
+let hello ='hello';
+console.log(hello)
+
+let owu = 'owu'
+console.log(owu)
+
+let com = 'com'
+console.log(com)
+
+let ua = 'ua'
+console.log(ua)
+
+let one = 1
+console.log(1)
+
+let ten = 10
+console.log(10)
+
+let hundred = -999
+console.log(-999)
+
+let number = 123
+console.log(123)
+
+let pi = 3.14
+console.log(3.14)
+
+let number2 = 2.7
+console.log(2.7)
+
+let sixteen = 16
+console.log(16)
+
+let t = true
+console.log(true)
+
+let f =false
+console.log(false)
+
+
+
+let firstName = 'Denys'
+let middleName = 'Viktorovich'
+let lastName = 'Pochka'
+console.log(`${firstName} ${middleName} ${lastName}`)
+
+
+
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
